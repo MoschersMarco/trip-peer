f175e383dc93ee969b1156a19ddbf7df0d8d9215
mmoscher <Marco.Moscher@inform-software.com>
2015-09-17 20:08:59 +0200

    First camera stuff

--- app/src/main/java/com/example/schake/trip_peer/FotoNeu.java
+++ app/src/main/java/com/example/schake/trip_peer/FotoNeu.java
@@ -1,16 +1,33 @@
 package com.example.schake.trip_peer;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.schake.trip_peer.Camera.CameraPreview;
 
 public class FotoNeu extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foto_neu);
+
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
 
     @Override
@@ -33,5 +50,17 @@ public class FotoNeu extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            Log.e("FotoNeu", "No Camera found!");
+        }
+        return c; // returns null if camera is unavailable
     }
 }
--- app/src/main/java/com/example/schake/trip_peer/Hauptmenu.java
+++ app/src/main/java/com/example/schake/trip_peer/Hauptmenu.java
@@ -1,16 +1,40 @@
 package com.example.schake.trip_peer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Hauptmenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hauptmenu);
+    }
+
+
+    public void newPicture( View view ) {
+        Intent newPictureIntent = new Intent(this, FotoNeu.class );
+        startActivity(newPictureIntent);
+    }
+
+    public void currentTrip( View view ) {
+        Intent newPictureIntent = new Intent(this, UrlaubAktuell.class );
+        startActivity( newPictureIntent );
+    }
+
+    public void newTrip( View v ) {
+        Intent newPictureIntent = new Intent(this, UrlaubNeu.class );
+        startActivity( newPictureIntent );
+    }
+
+    public void latestTrips( View view ) {
+        Intent newPictureIntent = new Intent(this, UrlaubArchiviert.class );
+        startActivity( newPictureIntent );
     }
 
     @Override
