8889c115508c1a7ae0ceb4f94a5c8326fd8e31a4
mmoscher <Marco.Moscher@inform-software.com>
2015-09-18 10:14:13 +0200

    Open Changes

--- app/src/main/java/com/example/schake/trip_peer/UrlaubUebersicht.java
+++ app/src/main/java/com/example/schake/trip_peer/UrlaubUebersicht.java
@@ -1,13 +1,41 @@
 package com.example.schake.trip_peer;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class UrlaubUebersicht extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class UrlaubUebersicht extends FragmentActivity
+            implements OnMapReadyCallback {
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+        // Instantiates a new Polyline object and adds points to define a rectangle
+        PolylineOptions rectOptions = new PolylineOptions()
+                .add(new LatLng(37.35, -122.0))
+                .add(new LatLng(37.45, -122.0))  // North of the previous point, but at the same longitude
+                .add(new LatLng(37.45, -122.2))  // Same latitude, and 30km to the west
+                .add(new LatLng(37.35, -122.2))  // Same longitude, and 16km to the south
+                .add(new LatLng(37.35, -122.0)); // Closes the polyline.
+
+
+        // Get back the mutable Polyline
+        Polyline polyline = map.addPolyline(rectOptions);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/schake/trip_peer/utils/ArchivedTripListClickHandler.java
+++ app/src/main/java/com/example/schake/trip_peer/utils/ArchivedTripListClickHandler.java
@@ -5,6 +5,7 @@ import android.view.View;
 
 import com.example.schake.trip_peer.UrlaubArchiviert;
 import com.example.schake.trip_peer.UrlaubFoto;
+import com.example.schake.trip_peer.UrlaubUebersicht;
 
 public class ArchivedTripListClickHandler implements View.OnClickListener {
 
@@ -16,7 +17,7 @@ public class ArchivedTripListClickHandler implements View.OnClickListener {
     @Override
     public void onClick(View v)
     {
-       Intent showPicturesIntent = new Intent(v.getContext(), UrlaubFoto.class );
+       Intent showPicturesIntent = new Intent(v.getContext(), UrlaubUebersicht.class );
         showPicturesIntent.putExtra("tripId", this.tripId );
         v.getContext().startActivity( showPicturesIntent );
     }
